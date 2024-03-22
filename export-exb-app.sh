cb48b4e3625c63d29425aa5e4d9dc1106e070c94
DavidSpriggs <spriggs.d@gmail.com>
2024-03-22 11:47:24 -0500

    updates

--- export-exb-app.sh
+++ export-exb-app.sh
@@ -9,6 +9,6 @@ read clientId
 echo "Enter the filename to save as (leave off .zip):"
 read filename
 
-docker exec arcgis-exb node -e "require('./server/src/middlewares/dev/apps/app-download.js').zipApp('$appnum', 'app.zip', '$clientId')"
+docker exec -e NODE_ENV=production arcgis-exb node -e "require('./server/src/middlewares/dev/apps/app-download.js').zipApp('$appnum', 'app.zip', '$clientId')"
 docker cp arcgis-exb:/home/node/ArcGISExperienceBuilder/app.zip ./$filename.zip
 docker exec arcgis-exb rm -f app.zip
